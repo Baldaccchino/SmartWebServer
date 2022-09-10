1d75981ba600ce605f99162501e303738b7d4afa
Josh Gill <josh.dgill@gmail.com>
2022-09-10 16:24:45 -0700

    reogranize app.vue to match usual format

--- src/pages/sws-ui/src/App.vue
+++ src/pages/sws-ui/src/App.vue
@@ -1,3 +1,33 @@
+<template lang="pug">
+FullScreen(v-if="networkError")
+  .flex.justify-center
+    NetworkDown
+
+FullScreen(v-else-if="!status")
+  LoadingSpinner
+    | Connecting to OnStep...
+
+FullScreen(v-else-if="status.type === 'invalid'")
+  .flex.justify-center
+    SerialDown
+
+.min-h-screen.flex.flex-col(v-else-if="status")
+  Navbar.mb-5(
+    :version="status.version"
+    :control="control"
+    :status="status"
+  )
+  RouterView.mx-2.2xl_mx-60.xl_mx-40.md_mx-20.sm_mx-10.grow(
+    v-slot="{ Component }"
+  )
+    component(
+      :status="status"
+      :control="control"
+      :is="Component"
+    )
+
+</template>
+
 <script setup lang="ts">
 import { ref, onBeforeUnmount } from "vue";
 import { useRouter } from "vue-router";
@@ -36,35 +66,3 @@ const control = new MountControl(onStep, onError)
   // we'll push them to the control page after a goto completion.
   .onAfterGoto(() => router.push({ name: "control" }));
 </script>
-
-<template lang="pug">
-FullScreen(v-if="networkError")
-  .flex.justify-center
-    NetworkDown
-
-FullScreen(v-else-if="!status")
-  LoadingSpinner
-    | connecting..
-
-FullScreen(v-else-if="status.type === 'invalid'")
-  .flex.justify-center
-    SerialDown
-
-.min-h-screen.flex.flex-col(v-else-if="status")
-  Navbar.mb-5(
-    :version="status.version"
-    :control="control"
-    :status="status"
-  )
-  RouterView.mx-2.2xl_mx-60.xl_mx-40.md_mx-20.sm_mx-10.grow(
-    v-slot="{ Component }"
-  )
-    component(
-      :status="status"
-      :control="control"
-      :is="Component"
-    )
-
-</template>
-
-<style scoped></style>
